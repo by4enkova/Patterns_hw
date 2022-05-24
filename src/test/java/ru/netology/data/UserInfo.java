6a2f6cae168cc8c2938941a6b707feab17e08f54
Darya Bychenkova <by4enkova1@gmail.com>
2022-05-24 20:53:12 +0300

    Test is added

---
+++ src/test/java/ru/netology/data/UserInfo.java
@@ -0,0 +1,13 @@
+package ru.netology.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserInfo {
+    private final String city;
+    private final String name;
+    private final String phone;
+}
+
